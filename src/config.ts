e67ef0f430487c32c986b5b01db086ab55903dda
Yoryoboy <93jads@gmail.com>
2025-09-05 15:22:45 -0300

    feat: add support for separate dev/prod AFIP certificates and access token

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,10 @@ export default {
   CUIT: process.env.AFIP_CUIT!,
   PASSWORD: process.env.AFIP_PASSWORD!,
   CERT_ALIAS: process.env.AFIP_CERT_ALIAS!,
-  CERT_PATH: process.env.AFIP_CERT_PATH!,
-  KEY_PATH: process.env.AFIP_KEY_PATH!,
+  DEV_CERT_PATH: process.env.AFIP_DEV_CERT_PATH!,
+  DEV_KEY_PATH: process.env.AFIP_DEV_KEY_PATH!,
+  PROD_CERT_PATH: process.env.AFIP_PROD_CERT_PATH!,
+  PROD_KEY_PATH: process.env.AFIP_PROD_KEY_PATH!,
   AFIP_PRODUCTION: process.env.AFIP_PRODUCTION === "true",
+  ACCESS_TOKEN: process.env.AFIP_SDK_ACCESS_TOKEN!,
 };
--- src/services/afip/client.ts
+++ src/services/afip/client.ts
@@ -2,13 +2,29 @@ import Afip from "@afipsdk/afip.js";
 import fs from "fs";
 import config from "../../config";
 
-const { CUIT, CERT_PATH, KEY_PATH } = config;
-
-const afip = new Afip({
-  cert: fs.readFileSync(CERT_PATH, "utf8"),
-  key: fs.readFileSync(KEY_PATH, "utf8"),
+const {
   CUIT,
-  production: false,
-});
+  DEV_CERT_PATH,
+  DEV_KEY_PATH,
+  AFIP_PRODUCTION,
+  ACCESS_TOKEN,
+  PROD_CERT_PATH,
+  PROD_KEY_PATH,
+} = config;
+
+const afip = AFIP_PRODUCTION
+  ? new Afip({
+      cert: fs.readFileSync(PROD_CERT_PATH, "utf8"),
+      key: fs.readFileSync(PROD_KEY_PATH, "utf8"),
+      CUIT,
+      production: AFIP_PRODUCTION,
+      access_token: ACCESS_TOKEN,
+    })
+  : new Afip({
+      cert: fs.readFileSync(DEV_CERT_PATH, "utf8"),
+      key: fs.readFileSync(DEV_KEY_PATH, "utf8"),
+      CUIT,
+      production: AFIP_PRODUCTION,
+    });
 
 export default afip;
